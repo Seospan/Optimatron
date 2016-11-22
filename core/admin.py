4bfa9df1383be9545e1bc7bb574d7cf60fc4d9cc
Seospan <sophie.jobez@gmail.com>
2016-11-22 15:17:02 +0100

    Django create core + snippet

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Snippet
+
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+    list_display = ('titre', 'slug', 'pub_date')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 # Create your models here.
+
+class Snippet(models.Model):
+    titre = models.CharField(max_length=320)
+    slug = models.SlugField(unique=True,primary_key=True)
+    contenu = HTMLField()
+    pub_date = models.DateTimeField('date de publication')
+
+    def __str__(self):
+        return self.titre
